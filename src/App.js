647ab203be3e2602b9a3007d3efe90bf39a3c707
chanhee won <wch2208@gmail.com>
2024-02-01 23:38:25 +0900

    MUI 설치

--- src/App.js
+++ src/App.js
@@ -2,14 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>test</p>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;

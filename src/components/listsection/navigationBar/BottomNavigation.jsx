dcd06c73f6ce7f7daff14e75b632e98e07d57289
chanhee won <wch2208@gmail.com>
2024-02-03 12:42:55 +0900

    api test

--- src/components/listsection/navigationBar/BottomNavigation.jsx
+++ src/components/listsection/navigationBar/BottomNavigation.jsx
@@ -7,6 +7,26 @@ import HomeIcon from "@mui/icons-material/Home";
 export default function SimpleBottomNavigation({ scrollToTop }) {
   const [value, setValue] = React.useState(0);
 
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://family-album-three.vercel.app/api/mediaList",
+        {
+          method: "GET", // HTTP 메소드 지정
+          headers: {
+            // 헤더에 Authorization 추가
+            Authorization: "fiKyi3A8FTuyq65cdObDsodi",
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log("요청 중 에러가 발생했습니다.", error);
+    }
+  };
+
   return (
     <Box sx={{ width: "100%" }}>
       <BottomNavigation
@@ -15,6 +35,7 @@ export default function SimpleBottomNavigation({ scrollToTop }) {
         onChange={(event, newValue) => {
           setValue(newValue);
         }}
+        onClick={fetchData}
       >
         <BottomNavigationAction
           label="Home"
